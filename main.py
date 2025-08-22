073732bf3aa226bc6984bd9690f6c8bc7d00e2e5
Ryan Forg <ryanforgit@gmail.com>
2025-08-22 23:31:24 +0100

    adds main

---
+++ main.py
@@ -0,0 +1,8 @@
+# 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
+
+def main():
+	print("hello world!")
+
+
+if __name__ == "__main__":
+	main()
